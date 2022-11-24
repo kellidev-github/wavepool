9c818d71596d82883450a3514dadc42e76fab953
kellidev-github <100567825+kellidev-github@users.noreply.github.com>
2022-11-24 14:20:29 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,29 @@
+@@ -1,17 +1,17 @@
 // 2D Water Ripples
 // The Coding Train / Daniel Shiffman
 // https://thecodingtrain.com/CodingChallenges/102-2d-water-ripple.html
 // https://youtu.be/BZUdGqeOD0w
 // https://editor.p5js.org/codingtrain/sketches/tYXtzNSl
-
 // Algorithm: https://web.archive.org/web/20160418004149/http://freespace.virgin.net/hugo.elias/graphics/x_water.htm
-
 let cols;
 let rows;
 let current; // = new float[cols][rows];
 let previous; // = new float[cols][rows];
 
-let dampening = 0.99;
+let dampening = 0.999;
 
+// load two soundfile and crossfade beetween them
+let sound1,sound2;
+function preload(){
+  soundFormats('ogg', 'mp3');
+  sound1 = loadSound('Underwater_Pool-Mike_Koenig-355864284.mp3');
+  sound2 = loadSound('Children Playing-SoundBible.com-591301488.mp3');
+  
+}
 function setup() {
   pixelDensity(1);
-  createCanvas(500, 600);
+  let cnv = createCanvas(500, 700);
+  cnv.mousePressed(startSound);
   cols = width;
   rows = height;
   // The following line initializes a 2D cols-by-rows array with zeroes
@@ -24,14 +32,20 @@ function setup() {
   current = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
   previous = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
 }
-
 function mouseDragged() {
   previous[mouseX][mouseY] = 2500;
 }
-
+function startSound() {
+  sound1.loop();
+  sound2.loop();
+  loop();
+}
+function mouseReleased() {
+//   sound1.stop();
+  sound2.pause();
+}
 function draw() {
   background(0);
-
   loadPixels();
   for (let i = 1; i < cols - 1; i++) {
     for (let j = 1; j < rows - 1; j++) {
@@ -48,16 +62,14 @@ function draw() {
       // set the entries for each color component separately.
       let index = (i + j * cols) * 4;
       pixels[index + 0] = 102+current[i][j];
-      pixels[index + 1] = 255;
-      pixels[index + 2] = 255;
+      pixels[index + 1] = 200+current[i][j];
+      pixels[index + 2] = 200+current[i][j];
     }
   }
   updatePixels();
-
   let temp = previous;
   previous = current;
   current = temp;
-
   
 }
 
