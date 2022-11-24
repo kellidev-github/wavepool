95ec4c2c66aa138d8ce9d28492fff74ad3ab4d5f
kellidev-github <100567825+kellidev-github@users.noreply.github.com>
2022-11-23 22:59:30 -0800

    Pool with 1 sound when pressed

--- sketch.js
+++ sketch.js
@@ -1,59 +1,85 @@
+// 2D Water Ripples
+// The Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/102-2d-water-ripple.html
+// https://youtu.be/BZUdGqeOD0w
+// https://editor.p5js.org/codingtrain/sketches/tYXtzNSl
+
+// Algorithm: https://web.archive.org/web/20160418004149/http://freespace.virgin.net/hugo.elias/graphics/x_water.htm
+
+let cols;
+let rows;
+let current; // = new float[cols][rows];
+let previous; // = new float[cols][rows];
+
+let dampening = 0.99;
+
 // load two soundfile and crossfade beetween them
 let sound1,sound2;
-let sound1Gain, sound2Gain, mixGain;
+
 function preload(){
   soundFormats('ogg', 'mp3');
   sound1 = loadSound('Underwater_Pool-Mike_Koenig-355864284.mp3');
   sound2 = loadSound('Children Playing-SoundBible.com-591301488.mp3');
   
 }
+
 function setup() {
-  let cnv = createCanvas(100, 100);
+  pixelDensity(1);
+  let cnv = createCanvas(500, 600);
   cnv.mousePressed(startSound);
-  // create a 'mix' gain bus to which we will connect both soundfiles
-    mixGain = new p5.Gain();
-  mixGain.connect();
-  sound1.disconnect(); // diconnect from p5 output
-  sound1Gain = new p5.Gain(); // setup a gain node
-  sound1Gain.setInput(sound1); // connect the first sound to its input
-  sound1Gain.connect(mixGain); // connect its output to the final mix bus
-  sound2.disconnect();
-  sound2Gain = new p5.Gain();
-  sound2Gain.setInput(sound2);
-  sound2Gain.connect(mixGain);
-    
+  cols = width;
+  rows = height;
+  // The following line initializes a 2D cols-by-rows array with zeroes
+  // in every array cell, and is equivalent to this Processing line:
+  // current = new float[cols][rows];
+  current = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
+  previous = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
 }
+
+function mouseDragged() {
+  previous[mouseX][mouseY] = 2500;
+}
+
 function startSound() {
-  sound1.loop();
+//   sound1.loop();
   sound2.loop();
   loop();
 }
+
 function mouseReleased() {
-  sound1.stop();
+//   sound1.stop();
   sound2.stop();
 }
-function draw(){
-  background(220);
-  textAlign(CENTER);
-  textSize(11);
-  fill(0);
-  if (!sound2.isPlaying()) {
-    text('tap and drag to play', width/2, height/2);
-    sound2.play();
-    return;
+
+function draw() {
+  background(0);
+
+  loadPixels();
+  for (let i = 1; i < cols - 1; i++) {
+    for (let j = 1; j < rows - 1; j++) {
+      current[i][j] =
+        (previous[i - 1][j] +
+          previous[i + 1][j] +
+          previous[i][j - 1] +
+          previous[i][j + 1]) /
+          2 -
+        current[i][j];
+      current[i][j] = current[i][j] * dampening;
+      // Unlike in Processing, the pixels array in p5.js has 4 entries
+      // for each pixel, so we have to multiply the index by 4 and then
+      // set the entries for each color component separately.
+      let index = (i + j * cols) * 4;
+      pixels[index + 0] = 102+current[i][j];
+      pixels[index + 1] = 255;
+      pixels[index + 2] = 255;
+    }
   }
-  // map the horizontal position of the mouse to values useable for volume    *  control of sound1
-  var sound1Volume = constrain(map(mouseX,width,0,0,1), 0, 1);
-  var sound2Volume = 1-sound1Volume;
-  sound1Gain.amp(sound1Volume);
-  sound2Gain.amp(sound2Volume);
-  // map the vertical position of the mouse to values useable for 'output    *  volume control'
-  var outputVolume = constrain(map(mouseY,height,0,0,1), 0, 1);
-  mixGain.amp(outputVolume);
-  text('output', width/2, height - outputVolume * height * 0.9)
-  fill(255, 0, 255);
-//   textAlign(LEFT);
-//   text('sound1', 5, height - sound1Volume * height * 0.9);
-//   textAlign(RIGHT);
-//   text('sound2', width - 5, height - sound2Volume * height * 0.9);
+  updatePixels();
+
+  let temp = previous;
+  previous = current;
+  current = temp;
+
+  
 }
+
