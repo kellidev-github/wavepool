057d48caba8f8c1db939c0505e9180edbbb15d04
kellidev-github <100567825+kellidev-github@users.noreply.github.com>
2022-11-24 12:36:15 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,26 +3,20 @@
 // https://thecodingtrain.com/CodingChallenges/102-2d-water-ripple.html
 // https://youtu.be/BZUdGqeOD0w
 // https://editor.p5js.org/codingtrain/sketches/tYXtzNSl
-
 // Algorithm: https://web.archive.org/web/20160418004149/http://freespace.virgin.net/hugo.elias/graphics/x_water.htm
-
 let cols;
 let rows;
 let current; // = new float[cols][rows];
 let previous; // = new float[cols][rows];
-
 let dampening = 0.99;
-
 // load two soundfile and crossfade beetween them
 let sound1,sound2;
-
 function preload(){
   soundFormats('ogg', 'mp3');
   sound1 = loadSound('Underwater_Pool-Mike_Koenig-355864284.mp3');
   sound2 = loadSound('Children Playing-SoundBible.com-591301488.mp3');
   
 }
-
 function setup() {
   pixelDensity(1);
   let cnv = createCanvas(500, 700);
@@ -35,7 +29,6 @@ function setup() {
   current = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
   previous = new Array(cols).fill(0).map(n => new Array(rows).fill(0));
 }
-
 function mouseDragged() {
   previous[mouseX][mouseY] = 2500;
 }
@@ -45,15 +38,12 @@ function startSound() {
   sound2.loop();
   loop();
 }
-
 function mouseReleased() {
 //   sound1.stop();
   sound2.pause();
 }
-
 function draw() {
   background(0);
-
   loadPixels();
   for (let i = 1; i < cols - 1; i++) {
     for (let j = 1; j < rows - 1; j++) {
@@ -70,16 +60,13 @@ function draw() {
       // set the entries for each color component separately.
       let index = (i + j * cols) * 4;
       pixels[index + 0] = 102+current[i][j];
-      pixels[index + 1] = 250+current[i][j];
-      pixels[index + 2] = 250+current[i][j];
+      pixels[index + 1] = 255;
+      pixels[index + 2] = 255;
     }
   }
   updatePixels();
-
   let temp = previous;
   previous = current;
   current = temp;
-
   
 }
-
